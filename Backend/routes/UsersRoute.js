87799fc7f196b9eeb33cf22d181ab65ab152de3d
sarthak4399 <sarthakkhandare21@gmail.com>
2023-03-29 22:16:11 +0530

    test revert

--- Backend/routes/UsersRoute.js
+++ Backend/routes/UsersRoute.js
@@ -1,22 +1,21 @@
 const express =require('express')
 const router = express.Router()
-const {getUsers}= require('../controller/controller')
-// router.get('/', (req, res) => { //all is working fine like this 
-//     res.status(200).json({
-//         Users: " get users "
-//     })
-// })
-// router.post('/', (req, res) => {
-//     res.status(200).json({
-//         Users: " add users"
-//     })
-// })
-// router.get('/',getUsers) // this is not working 
-// router.post('/',getUsers)
+
+router.get('/', (req, res) => { 
+    res.status(200).json({
+        Users: " get users "
+    })
+})
+
+router.post('/', (req, res) => {
+    res.status(200).json({
+        Users: " add users"
+    })
+})
 
 router.put('/:id', (req, res) => {
     res.status(200).json({
-        Users: ` Update user ${ req.params.id} `
+        Users: `Update user ${ req.params.id}`
     })
 })
 
@@ -24,6 +23,7 @@ router.delete('/:id', (req, res) => {
     res.status(200).json({
         Users: `Delete user ${ req.params.id} `
     })
+
 })
 
 
