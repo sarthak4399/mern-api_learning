27ed0fab7089398eca27d2f3e64008744f5d8a86
sarthak4399 <sarthakkhandare21@gmail.com>
2023-03-29 21:44:31 +0530

    test

--- Backend/routes/UsersRoute.js
+++ Backend/routes/UsersRoute.js
@@ -1,9 +1,19 @@
 const express =require('express')
 const router = express.Router()
 const {getUsers}= require('../controller/controller')
-router.get('/', getUsers)
+// router.get('/', (req, res) => { //all is working fine like this 
+//     res.status(200).json({
+//         Users: " get users "
+//     })
+// })
+// router.post('/', (req, res) => {
+//     res.status(200).json({
+//         Users: " add users"
+//     })
+// })
+// router.get('/',getUsers) // this is not working 
+// router.post('/',getUsers)
 
-router.post('/', getUsers)
 router.put('/:id', (req, res) => {
     res.status(200).json({
         Users: ` Update user ${ req.params.id} `
